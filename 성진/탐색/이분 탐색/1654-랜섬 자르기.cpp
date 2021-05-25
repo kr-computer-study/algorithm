62219e068abb538a934ab4ba71bffb7f2f37a537
Thedum2 <76659528+Thedum2@users.noreply.github.com>
2021-05-25 17:14:09 +0900

    Create 1654-랜섬 자르기.cpp

---
+++ 성진/탐색/이분 탐색/1654-랜섬 자르기.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int K, N;
+vector<long> Lansun;
+
+bool Possible(long num) {
+	long result = 0;
+	for (int i = 0; i < K; i++)
+		result += Lansun[i] / num;
+
+		if (result >= N)
+			return true;
+		else
+			return false;
+}
+ int main()
+{
+	 cin >> K >> N;
+	 long x;
+	 long start =1, end = 0;
+	 for (int i = 0; i < K; i++)
+	 {
+		 cin >> x;
+		 Lansun.push_back(x);
+		 if (x > end)end = x;
+	 }
+
+	 long mid,result=0;
+	 while (start<=end)
+	 {
+		 mid = (start + end) / 2;
+		 if (Possible(mid) == true) {
+			 if (result < mid) result = mid;
+			 start = mid + 1;
+		 }
+		 else{
+			 end = mid - 1;
+		 }
+	 }
+	 cout << result;
+}
