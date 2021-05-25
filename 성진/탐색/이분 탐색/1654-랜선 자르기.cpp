2dd9a697012433778af278b8ec6f45900e542b10
Thedum2 <76659528+Thedum2@users.noreply.github.com>
2021-05-25 17:24:40 +0900

    들여쓰기

--- 성진/탐색/이분 탐색/1654-랜선 자르기.cpp
+++ 성진/탐색/이분 탐색/1654-랜선 자르기.cpp
@@ -10,31 +10,32 @@ bool Possible(long num) {
 	for (int i = 0; i < K; i++)
 		result += Lansun[i] / num;
 
-		if (result >= N) return true;
-		else return false;
+	if (result >= N) return true;
+	else return false;
 }
 
- int main(){
-	 cin >> K >> N;
-	 long x;
-	 long start =1, end = 0;
+int main() {
+	cin >> K >> N;
+	long x;
+	long start = 1, end = 0;
 
-	 for (int i = 0; i < K; i++){
-		 cin >> x;
-		 Lansun.push_back(x);
-		 if (x > end)end = x;
-	 }
+	for (int i = 0; i < K; i++) {
+		cin >> x;
+		Lansun.push_back(x);
+		if (x > end)end = x;
+	}
 
-	 long mid,result=0;
+	long mid, result = 0;
 
-	 while (start<=end){
-		 mid = (start + end) / 2;
-		 if (Possible(mid) == true) {
-			 if (result < mid) result = mid;
-			 start = mid + 1;
-		 }
-		 else
-			 end = mid - 1;
-	 }
-	 cout << result;
+	while (start <= end) {
+		mid = (start + end) / 2;
+		if (Possible(mid) == true) {
+			if (result < mid)
+				result = mid;
+			start = mid + 1;
+		}
+		else
+			end = mid - 1;
+	}
+	cout << result;
 }
